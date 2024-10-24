e596e1a24240c92fcab0d40c4aa97296721344c2
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-10-25 02:58:53 +0300

    LINQ - Set Operators

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -245,19 +245,34 @@ namespace Assignment
             //7. Select all orders where the order was made in 1998 or later.
 
             //var result = CustomersList.SelectMany(c => c.Orders, (c, o) => new { c.CustomerID, o.OrderID, o.OrderDate }).Where(o => o.OrderDate.Year >= 1998).ToList();
-            
+
             #endregion
 
             #region Set Operators
             //1.Find the unique Category names from Product List
 
-            var result = ProductsList.Select(p => p.Category).Distinct().ToList();
+            //var result = ProductsList.Select(p => p.Category).Distinct().ToList();
 
             //2.Produce a Sequence containing the unique first letter from both product and customer names.
+
+            //var result = ProductsList.Select(p => p.ProductName[0]).Distinct().Union(CustomersList.Select(c => c.CustomerName[0])).Distinct().ToList();
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
             //3.Create one sequence that contains the common first letter from both product and customer names.
+
+            //var result = ProductsList.Select(p => p.ProductName[0]).Intersect(CustomersList.Select(c => c.CustomerName[0])).ToList();
+
             //4.Create one sequence that contains the first letters of product names that are not also first letters of customer names.
+
+            //var result = ProductsList.Select(p => p.ProductName[0]).Except(CustomersList.Select(c => c.CustomerName[0])).ToList();
+
             //5.Create one sequence that contains the last Three Characters in each name of all customers and products, including any duplicates
 
+            //var result = ProductsList.Select(p => p.ProductName.Substring(p.ProductName.Length - 3)).Union(CustomersList.Select(c => c.CustomerName.Substring(c.CustomerName.Length - 3))).ToList();
             #endregion
 
             #region Partitioning Operators
